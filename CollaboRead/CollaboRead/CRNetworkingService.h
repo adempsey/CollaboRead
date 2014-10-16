63707159328120868cb09f9c61f1da937abace42
adempsey <amd0331@gmail.com>
2014-10-15 21:19:46 -0400

    created CRNetworkingService class

---
+++ CollaboRead/CollaboRead/CRNetworkingService.h
@@ -0,0 +1,16 @@
+//
+//  CRNetworkingService.h
+//  CollaboRead
+//
+//  Created by Andrew Dempsey on 10/15/14.
+//  Copyright (c) 2014 CollaboRead. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CRNetworkingService : NSObject
+
++ (CRNetworkingService*)sharedInstance;
+- (void)performGETRequestForResource:(NSString*)resource withParams:(NSDictionary*)params completionBlock:(void (^)(NSData*, NSURLResponse*, NSError*))completionBlock;
+
+@end
