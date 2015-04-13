7a81838c0095e7fcc39de228f5d9109c61b88341
hannahcclark <hannah_clark@mac.com>
2015-04-13 09:04:13 -0400

    collaborator list fix

--- CollaboRead/CollaboRead/Services/CRAccountService.m
+++ CollaboRead/CollaboRead/Services/CRAccountService.m
@@ -8,6 +8,7 @@
 
 #import "CRAccountService.h"
 #import "CRDrawingPreserver.h"
+#import "CRCollaboratorList.h"
 
 @implementation CRAccountService
 
@@ -21,11 +22,17 @@
 	return sharedInstance;
 }
 
+-(void)setUser:(CRUser *)user {
+    _user = user;
+    [[CRCollaboratorList sharedInstance] setOwner];
+}
+
 - (void)logout
 {
 	self.user = nil;
 	self.password = nil;
 	[[CRDrawingPreserver sharedInstance] clearDrawings];
+    [[CRCollaboratorList sharedInstance] clearCollaborators];
 }
 
 @end
